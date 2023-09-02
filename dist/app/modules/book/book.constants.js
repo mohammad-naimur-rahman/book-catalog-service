1147d72147d88dc738943d49f763531dee612f4a
Naimur <naeemhasan28@gmail.com>
2023-09-03 00:01:36 +0600

    try to fix: deploy problem for dist

--- dist/app/modules/book/book.constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bookSearchableFileds = exports.bookFilterableFields = void 0;
-exports.bookFilterableFields = ['category'];
-exports.bookSearchableFileds = ['title', 'author', 'genre'];
